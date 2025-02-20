3787fbfefdfeaf701e48e7f39817f24a16e2b08c
Nick Wall <149669706+nick-wall@users.noreply.github.com>
2025-02-20 16:41:06 +0000

    Update matching radios

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,19 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Direct from Your jobs page to either V1 straight to the annotation view or an alternative which has a sub taksk list for Entries
+
+router.post('/annotators/your-jobs-route', function(request, response) {
+
+    var jobsRoute = request.session.data['jobs-route']
+    if (jobsRoute == "V2"){
+        response.redirect("/ new page ")
+    } else {
+        response.redirect("/annotators/assignments/AV57267/entry-1/extent-1")
+    }
+})
+
+
+
+
